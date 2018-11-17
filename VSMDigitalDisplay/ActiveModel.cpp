df14ad367ee4983fa25b7618b2b1bf63b727c0c3
Martin Piper <martin.piper@gmail.com>
2018-11-17 16:30:48 +0800

    Stretch blit of random bitmap pixels during animation

--- VSMDigitalDisplay/ActiveModel.cpp
+++ VSMDigitalDisplay/ActiveModel.cpp
@@ -2,7 +2,7 @@
 #include "ActiveModel.h"
 #include "DsimModel.h"
 
-VOID ActiveModel::initialize (ICOMPONENT *cpt)
+VOID ActiveModel::initialize(ICOMPONENT *cpt)
 {
 	component = cpt;
 
@@ -15,10 +15,6 @@ VOID ActiveModel::initialize (ICOMPONENT *cpt)
 	volts.y = (textbox.top + textbox.bottom) / 2 - 100;
 	amps.x = (textbox.left + textbox.right) / 2;
 	amps.y = (textbox.top + textbox.bottom) / 2 + 100;
-
-	// Create a new text style in 'Templates' menu or other objects get funky
-	// Still makes other meters readouts conform to his style, how to use properly??
-	textstyle = cpt->createtextstyle((CHAR*)"UM_METER");
 
 	// Initial readout:
 	strcpy(readout_v, " V00.0");
@@ -48,13 +44,61 @@ VOID ActiveModel::plot (ACTIVESTATE state)
 
 VOID ActiveModel::animate (INT element, ACTIVEDATA *newstate)
 {
+	BOX textbox;
+	component->getsymbolarea(-1, &textbox);
+
 	// Draw entire component on the screen
-	component->drawsymbol(-1);
+//	component->drawsymbol(-1);
 	// Draw symbol 0 overtop components
-	component->drawsymbol(0);
+//	component->drawsymbol(0);
 	// Re-draw the displayed value with the new results
-	component->drawtext(volts.x, volts.y, 0, TXJ_CENTRE | TXJ_MIDDLE, readout_v);
-	component->drawtext(amps.x, amps.y, 0, TXJ_CENTRE | TXJ_MIDDLE, readout_a);
+//	component->drawtext(volts.x, volts.y, 0, TXJ_CENTRE | TXJ_MIDDLE, readout_v);
+//	component->drawtext(amps.x, amps.y, 0, TXJ_CENTRE | TXJ_MIDDLE, readout_a);
+
+	textbox.left += 470;
+	textbox.top += 270;
+	textbox.right -= 250;
+//	textbox.bottom += 300;
+	HDC hdc = component->begincache(textbox);
+
+	int pixels[32 * 32];
+	for (int i = 0; i < 32 * 32; i++)
+	{
+		pixels[i] = rand();
+	}
+
+	BITMAP bm;
+	PAINTSTRUCT ps;
+	HBITMAP bitmap = CreateBitmap(32 , 32 , 1 , 32 , pixels);
+
+	HDC hdcMem = CreateCompatibleDC(hdc);
+	HGDIOBJ hbmOld = SelectObject(hdcMem, bitmap);
+
+
+//	GetObject(bitmap, sizeof(bm), &bm);
+//	BitBlt(hdc, 0, 0, bm.bmWidth, bm.bmHeight, hdcMem, 0, 0, SRCCOPY);
+
+	HGDIOBJ hBitmap = GetCurrentObject(hdc, OBJ_BITMAP);
+	GetObject(hBitmap, sizeof(bm), &bm);
+	StretchBlt(hdc, 0, 0, bm.bmWidth, bm.bmHeight, hdcMem, 0, 0, 32 , 32, SRCCOPY);
+
+	SelectObject(hdcMem, hbmOld);
+	DeleteDC(hdcMem);
+
+	DeleteObject(bitmap);
+
+#if 0
+	MoveToEx(hDC, volts.x, volts.y, NULL);
+	LineTo(hDC, volts.x+50, volts.y+50);
+	COLORREF color = RGB(255, 0, 0); // COLORREF to hold the color info`
+	for (int i = 0; i < 400; i++)
+	{
+		for (int j = 0; j < 50; j++)
+			SetPixel(hDC, i, j, color);
+	}
+#endif
+
+	component->endcache();
 }
 
 BOOL ActiveModel::actuate (WORD key, INT x, INT y, DWORD flags)
